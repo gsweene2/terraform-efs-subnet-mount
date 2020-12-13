ade107f37f86219dc01ab68f5b5428a7b6498f41
Garrett Sweeney <garrett.d.sweeney@gmail.com>
2020-12-13 16:40:57 -0500

    conditional on encryped

--- main.tf
+++ main.tf
@@ -6,10 +6,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_efs_file_system" "foo_with_lifecyle_policy" {
+resource "aws_efs_file_system" "file_system" {
   creation_token                  = var.creation_token
   encrypted                       = var.encrypted
-  kms_key_id                      = var.kms_key_id != null ? var.kms_key_id : null
+  kms_key_id                      = var.encrypted && var.kms_key_id != null ? var.kms_key_id : null
   performance_mode                = var.performance_mode
   throughput_mode                 = var.throughput_mode
   provisioned_throughput_in_mibps = var.throughput_mode == "provisioned" ? var.provisioned_throughput_in_mibps : null
@@ -20,6 +20,6 @@ resource "aws_efs_file_system" "foo_with_lifecyle_policy" {
 }
 
 resource "aws_efs_mount_target" "alpha" {
-  file_system_id = aws_efs_file_system.foo_with_lifecyle_policy.id
+  file_system_id = aws_efs_file_system.file_system.id
   subnet_id      = var.subnet_id
 }
